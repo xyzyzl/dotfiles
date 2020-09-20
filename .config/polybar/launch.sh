1a7a61232dd0ef5ee40afa8f3f02735c7c0eed70
Albert Ye <41131127+xyzyzl@users.noreply.github.com>
2020-09-20 14:52:59 -0700

    allow multi-monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,2 +1,9 @@
 killall -q polybar
-polybar main &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
